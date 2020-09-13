eafd17b77c240ac3c0e752565c0f96e2a516afa7
Tyler Samuelson <tylersam@gmail.com>
2020-09-13 14:55:55 -0500

    Add Lawyer type and setup graphiql

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,18 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :lawyer, LawyerType, null: true do
+      description "Find a lawyer by ID"
+      argument :id, ID, required: true
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :lawyers, [LawyerType], null: true
+
+    def lawyer(id:)
+      Lawyer.find(id)
+    end
+
+    def lawyers
+      Lawyer.all.order(:id)
     end
   end
 end
